0ccb9dec64787e9a3ec4454c534fc287fcb1f54d
RyanSikandar <ryansikandar@gmail.com>
2025-02-10 00:01:24 +0500

    feat; somehtign

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const completed = require("./completed");
 const recipes = require("./recipes/api");
 const ingredients = require("./ingredients/api");
 const movies = require("./paginationPractice")
+const rateLimit = require('express-rate-limit')
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  message: "Too many requests from this IP, please try again after 15 minutes",
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
 
 const app = express();
-
 app.get("/", (_, res) => res.sendFile(path.join(__dirname, "./index.html")));
 app.get("/style.css", (_, res) =>
   res.sendFile(path.join(__dirname, "./style.css"))
@@ -20,7 +29,7 @@ app.use("/images", express.static("./images"));
 app.use("/completed", completed);
 app.use("/recipes", recipes);
 app.use("/ingredients", ingredients);
-app.use("/movies", movies);
+app.use("/movies", limiter, movies);
 
 app.get("/hello", (req, res) => res.json({ status: "ok" }));
 
